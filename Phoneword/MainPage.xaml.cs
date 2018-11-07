887533608a60da2baa9b66cdad0fbdf2f5831b96
darthvader <lxeguard@hotmail.com>
2018-11-07 17:21:05 +0800

    initial commit

---
+++ Phoneword/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Phoneword
+{
+    public partial class MainPage : ContentPage
+    {
+        string translatedNumber;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnTranslate(object sender, EventArgs e)
+        {
+            translatedNumber = PhonewordTranslator.
+        }
+    }
+}
---
+++ Phoneword/PhoneTranslator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+namespace Phoneword
+{
+    public static class PhoneTranslator
+    {
+        public PhoneTranslator()
+        {
+        }
+
+        public static string ToNumber(string raw) {
+            if (string.IsNullOrWhiteSpace(raw)) {
+                return null;
+            }
+
+            raw = raw.ToUpperInvariant();
+
+            var newNumber = new StringBuilder();
+            foreach (var c in raw) 
+            {
+                if (" -0123456789".Contains(c))
+                    newNumber.Append(c);
+                else
+                {
+                    var result = TranslateToNumber(c);
+                    if (result != null)
+                        newNumber.Append(result);
+                    else
+                        return null;
+                }
+            }
+            return newNumber.ToString();
+        }
+    }
+}
