1f7f3b3082680b69291984bd40da67f618aa7248
darthvader <lxeguard@hotmail.com>
2018-11-07 21:09:12 +0800

    add android platform specific phone dial

--- Phoneword.Android/MainActivity.cs
+++ Phoneword.Android/MainActivity.cs
@@ -12,12 +12,16 @@ namespace Phoneword.Droid
     [Activity(Label = "Phoneword", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        internal static MainActivity Instance { get; private set; }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
+            Instance = this;
+
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
