4433f407007ba873fa1da7006bbc777bae3b8967
darthvader <lxeguard@hotmail.com>
2018-11-09 01:14:03 +0800

    fix 2nd display of listview work bug

--- Phoneword/InstructionPracticePage.xaml.cs
+++ Phoneword/InstructionPracticePage.xaml.cs
@@ -7,8 +7,12 @@ namespace Phoneword
 {
     public partial class InstructionPracticePage : ContentPage
     {
+        public static string[] BuyList { get; set; }
         public InstructionPracticePage()
         {
+            BuyList = new string[] {
+                "Buy pears", "Buy oranges", "Buy mangos", "Buy apples", "Buy bananas"
+            };
             InitializeComponent();
         }
     }
